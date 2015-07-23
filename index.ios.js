5a965f80fc8028cb86f46cc8ae67b9c216352d6f
Jay Meistrich <jmeistrich@gmail.com>
2015-07-23 10:35:44 +0200

    Printing out the list of images

--- index.ios.js
+++ index.ios.js
@@ -19,15 +19,74 @@ var ddpClient = new DDPClient({url: 'ws://hp-photos-from-slack.meteor.com/websoc
 
 ddpClient.connect(() => ddpClient.subscribe('photos'));
 
+var timeout = undefined;
+
+var arr = [];
+
+var printEverything = function()
+{
+  var num = 0;
+  var rows = ddpClient.collections.photos;
+  for (var key in rows)
+  {
+    if (rows.hasOwnProperty(key))
+    {
+      var obj = rows[key];
+      if (obj.slack && obj.slack.filetype)
+      {
+        switch(obj.slack.filetype)
+        {
+          case 'jpg':
+          case 'jpeg':
+          case 'png':
+            arr.push(obj.slack.title);
+            num ++;
+            break;
+        }
+      }
+    }
+  }
+  console.log(arr);
+  console.log('num', num);
+}
+
     var updateRows = function(rows) {
-        console.log(rows);
+        var num = 0;
+        for (var key in rows)
+        {
+          if (rows.hasOwnProperty(key))
+          {
+            var obj = rows[key];
+            if (obj.slack && obj.slack.filetype)
+            {
+              switch(obj.slack.filetype)
+              {
+                case 'jpg':
+                case 'jpeg':
+                case 'png':
+                case 'gif':
+                  num ++;
+                  break;
+              }
+            }
+          }
+        }
+        if (timeout)
+        {
+          clearTimeout(timeout);
+        }
+        timeout = setTimeout(printEverything, 5000);
+        console.log('num photos', num);
       };
 
 // observe the lists collection
   var observer = ddpClient.observe("photos");
-  observer.added = () => updateRows(_.cloneDeep(_.values(ddpClient.collections.photos)));
-  observer.changed = () => updateRows(_.cloneDeep(_.values(ddpClient.collections.photos)));
-  observer.removed = () => updateRows(_.cloneDeep(_.values(ddpClient.collections.photos)));
+  observer.added = () => updateRows(ddpClient.collections.photos);
+  // observer.changed = () => updateRows(ddpClient.collections.photos);
+  // observer.removed = () => updateRows(ddpClient.collections.photos);
+  // observer.added = () => updateRows(_.cloneDeep(_.values(ddpClient.collections.photos)));
+  // observer.changed = () => updateRows(_.cloneDeep(_.values(ddpClient.collections.photos)));
+  // observer.removed = () => updateRows(_.cloneDeep(_.values(ddpClient.collections.photos)));
 
 
 
@@ -40,7 +99,7 @@ var hpPhotosIOS = React.createClass({
           Welcome to React Native!
         </Text>
         <Text style={styles.instructions}>
-          To get aksdhksadj, edit index.ios.js
+          To get z, edit index.ios.js
         </Text>
         <Text style={styles.instructions}>
           Press Cmd+R to reload,{'\n'}
